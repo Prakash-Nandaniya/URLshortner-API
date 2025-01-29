8d68269ebc12e922473000e741679b5fde03b262
prakashnandaniya12@gmail.com <Prakash-Nandaniya>
2025-01-29 20:25:52 +0530

    edited

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ connectDB().then(() => {
 });
 
 
-const corsOptions = {
-    origin:true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'],
+app.options('*', cors()); // Enable preflight requests for all routes
+app.use(cors({
+    origin: 'http://localhost:5173', // Or '*'
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-};
-app.use(cors(corsOptions));
+}));
 
 
 app.param('id', (req, res, next, id) => {
