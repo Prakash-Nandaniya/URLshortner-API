c5ecbb1091788aabd06636b6c349e84fa0f0df5c
Prakash Nandaniya <120181203+Prakash-Nandaniya@users.noreply.github.com>
2025-01-29 17:07:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(logReqRes);
 
 
 const corsOptions = {
-    origin: true,
+    origin:'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
