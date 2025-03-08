50ecb7f29e0045f03a9d91482f90352a919194f3
Prakash-Nandaniya <prakashnandaniya12@gmail.com>
2025-03-08 18:01:23 +0530

    -

--- controllers/user/home.js
+++ controllers/user/home.js
@@ -4,8 +4,8 @@ import user from '../../models/user.js';
 export async function userHome(req, res) {
     try {
         const userId = req.params.id;
-        const registered_urls = await Url
-            .find({ created_by: userId }, { _id: 1, url: 1, short_url: 1, clicks_count: 1 }) // Select specific fields
+        const registered_urls = await url
+            .find({ created_by: userId }, { _id: 1, url: 1, short_url: 1, clicks_count: 1 })
             .lean() 
             .exec();
         const user_profile = await user.findOne({ _id: userId }).lean();
